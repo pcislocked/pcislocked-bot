e48433f8fa52c6e5ea64d3c96eb184826566a0d5
pcislocked <50513999+pcislocked@users.noreply.github.com>
2020-11-25 22:46:47 +0300

    Update bot-detokenified.py

--- bot-detokenified.py
+++ bot-detokenified.py
@@ -229,5 +229,8 @@ async def on_message(message):
         else:
             await message.add_reaction('<:tore:739979995094712504>')
             
+    if message.content.lower() == 'pu' or message.content.lower() == 'pü':
+        await message.channel.send('https://media.giphy.com/media/3o6Mb6n1senEQtbgdy/giphy.gif')
+        
 client.run(TOKEN)
 client.run(TOKEN)
